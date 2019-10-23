d6f4afe3ec1509306c57e79afe62d2fbcd07e6a6
Giovana1523 <giovana1523@gmail.com>
2019-10-23 13:45:21 -0300

    .venv removido.

--- main/__init__.py
+++ main/__init__.py
@@ -2,21 +2,52 @@ from flask import Flask, Blueprint, flash, g, redirect, render_template, request
 from flask_socketio import SocketIO
 
 def create_app():
+    
+    jogador = ("x", "o")
+    vez = 1;
+
+    matriz_jogadores = [['-', 'x', '-', 'x', '-', 'x', '-', 'x', '-', 'x'],
+                        ['x', '-', 'x', '-', 'x', '-', 'x', '-', 'x', '-'],
+                        ['-', 'x', '-', 'x', '-', 'x', '-', 'x', '-', 'x'],
+                        ['x', '-', 'x', '-', 'x', '-', 'x', '-', 'x', '-'],
+                        ['-', '-', '-', '-', '-', '-', '-', '-', '-', '-'],
+                        ['-', '-', '-', '-', '-', '-', '-', '-', '-', '-'],
+                        ['-', 'o', '-', 'o', '-', 'o', '-', 'o', '-', 'o'],
+                        ['o', '-', 'o', '-', 'o', '-', 'o', '-', 'o', '-'],
+                        ['-', 'o', '-', 'o', '-', 'o', '-', 'o', '-', 'o'],
+                        ['o', '-', 'o', '-', 'o', '-', 'o', '-', 'o', '-']]
 
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'secret!'
     socketio = SocketIO(app)
 
-    @socketio.on('my event')
+    @socketio.on('handle click')
     def handle_message(message):
-        print('received message: ' + message["data"])
+        data = message["data"]
+        # cedula_selecionada = (data['i'], data['j'])
+        print (matriz_jogadores [data['j']][data['i']])
+        # socketio.emit('seleciona peca', {"data": data})
+        @socketio.on('handle click')
+        def handle_message(message):
+            data = message["data"]
+            i = data['i']
+            j = data['j']
+            # cedula_selecionada = (data['i'], data['j'])
+            print (matriz_jogadores [j][i])           
+            # socketio.emit('seleciona peca', {"data": data})
+            if matriz_jogadores[j][i] != '-':
+                socketio.emit('seleciona peca', {"data": data})
 
-    @socketio.on('my event', namespace='/test')
-    def handle_my_custom_namespace_event(json):
-        print('received json: ' + str(json))
+    # @socketio.on('connect')
+    # def handle_connect():
+    #     global jogadores
+    #     jogadores += 1
+    #     print(jogadores)
+    #     if jogadores == 2:
+    #         socketio.emit('mostra tabuleiro');
 
     @app.route('/')
     def home():
-        return render_template('home.html')
+        return render_template('index.html')
 
     return app
